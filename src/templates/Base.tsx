421a54fb0157d27b143ccd13b91bf9fb78c9a85d
João Pedro Rangel de Gomes e Silva <joao.rangelgs@gmail.com>
2024-09-25 20:36:03 -0500

    wrapped all components in the background color  provider

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -5,15 +5,18 @@ import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { Pictures } from './Pictures';
 import { VerticalFeatures } from './VerticalFeatures';
+import { Background } from '../background/Background';
 
 const Base = () => (
   <div className="text-golden-100 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
-    <Hero />
-    <Pictures />
-    <VerticalFeatures />
-    <Banner />
-    <Footer />
+    <Background color="bg-primary-100">
+      <Hero />
+      <Pictures />
+      <VerticalFeatures />
+      <Banner />
+      <Footer />
+    </Background>
   </div>
 );
 
