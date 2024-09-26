24a3502e01ede06b8ea3fc8cf3ddf1b6c9835f92
João Pedro Rangel de Gomes e Silva <joao.rangelgs@gmail.com>
2024-09-26 10:39:02 -0500

    deleted contact form. TODO: add contact form to contact page

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { CTABanner } from '../cta/CTABanner';
 import { Section } from '../layout/Section';
-import ContactPopup from '@/components/ContactPopup';
 
 const Banner = () => (
   <Section>
     <CTABanner
       title="Contact us for more information."
       subtitle="We would love to hear from you"
-      button={
-          <ContactPopup />
+      button={""
       }
     />
   </Section>
