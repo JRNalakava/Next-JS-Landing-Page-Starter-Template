b1603fdfd7520820d5a2fa4865d0a57b7f27ca50
João Pedro Rangel de Gomes e Silva <joao.rangelgs@gmail.com>
2024-09-29 17:18:49 -0500

    moved arrow function to top of file

--- src/components/RecommendationsCarousel.tsx
+++ src/components/RecommendationsCarousel.tsx
@@ -5,6 +5,32 @@ import React, { useState } from 'react';
 
 import { IPlayer } from './IPlayer';
 
+function Arrow(props: {
+  disabled: boolean;
+  left?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick: (e: any) => void;
+}) {
+  const disabled = props.disabled ? ' arrow--disabled' : '';
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? 'arrow--left' : 'arrow--right'
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  );
+}
+
 export default function App() {
   const [currentSlide, setCurrentSlide] = React.useState(0);
   const [loaded, setLoaded] = useState(false);
@@ -116,28 +142,3 @@ export default function App() {
   );
 }
 
-function Arrow(props: {
-  disabled: boolean;
-  left?: boolean;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  onClick: (e: any) => void;
-}) {
-  const disabled = props.disabled ? ' arrow--disabled' : '';
-  return (
-    <svg
-      onClick={props.onClick}
-      className={`arrow ${
-        props.left ? 'arrow--left' : 'arrow--right'
-      } ${disabled}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-      )}
-      {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-      )}
-    </svg>
-  );
-}
