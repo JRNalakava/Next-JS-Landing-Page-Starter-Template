f7753617056f93868a45d2c9a398ba0d72553660
João Pedro Rangel de Gomes e Silva <joao.rangelgs@gmail.com>
2024-10-01 15:37:45 -0500

    reenabled console logs. TODO: look into replacing these console statements

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -5,6 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const API_KEY = process.env.MAILGUN_API_KEY || '';
 const DOMAIN = process.env.MAILGUN_DOMAIN || '';
 
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -32,10 +33,10 @@ export default async function handler(
 
   try {
     const emailRes = await client.messages.create(DOMAIN, messageData);
-    //console.log(emailRes);
+    console.log(emailRes);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
-    //console.error('Error sending email', err);
+    console.error('Error sending email', err);
   }
 
   res.status(200).json({ submitted: true });
